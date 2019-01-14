4a44b0683fb931cc72e9758d4d13d1b7f44a5a49
nilcsi <nilcsi@gmail.com>
2019-01-14 14:25:14 +0100

    c1

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -1,4 +1,11 @@
 package test;
 
 public class Hello {
+  public static String cheer() {
+    return "Hello rest of Tiptop!";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(cheer());
+  }
 }
