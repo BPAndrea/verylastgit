27f7ed37f3d77ef02a4324e57bdd7a7a62e6e0ae
nilcsi <nilcsi@gmail.com>
2019-01-14 15:04:27 +0100

    Modified greet method

--- src/com/greenfox/Greet.java
+++ src/com/greenfox/Greet.java
@@ -1,9 +1,10 @@
 package com.greenfox;
 
 public class Greet {
-  public static void greet(String name, String otherName){
+  public static void greet(String name, String otherName, int number){
     System.out.println("Helló, " + name);
     System.out.println("Helló, " + otherName);
+    System.out.println(number);
   }
 
 
