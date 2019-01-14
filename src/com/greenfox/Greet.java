91070ae0ab116f9eed9d3e0afd3121e1f269b3fd
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-01-14 14:32:06 +0100

    a2 added parameter

--- src/com/greenfox/Greet.java
+++ src/com/greenfox/Greet.java
@@ -1,11 +1,12 @@
 package com.greenfox;
 
 public class Greet {
-  public static void greet(){
-    System.out.println("Helló, Bonnyai-Pap Andrea");
+  public static void greet(String name){
+
+    System.out.println("Helló, " + name);
   }
 
   public static void main(String[] args) {
-    greet();
+    greet("Nilcsi");
   }
 }
