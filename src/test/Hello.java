d4b7dd32553a410221b6cb8c554750d1c1e5a842
nilcsi <nilcsi@gmail.com>
2019-01-14 15:21:56 +0100

    maléwem

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -1,11 +1,11 @@
 package test;
 
 public class Hello {
-  public static String cheer(String hello, int number) {
-    return hello + number;
+  public static String cheer(String hello) {
+    return hello;
   }
 
   public static void main(String[] args) {
-    System.out.println(cheer("Hello rest of Tiptop! ", 10));
+    System.out.println(cheer("Hello rest of Tiptop! mlksdngnj"));
   }
 }
