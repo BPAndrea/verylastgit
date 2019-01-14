340880a4ec2bda984807be89e816d04e2c404168
nilcsi <nilcsi@gmail.com>
2019-01-14 14:32:08 +0100

    b2

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -1,11 +1,11 @@
 package test;
 
 public class Hello {
-  public static String cheer() {
-    return "Hello rest of Tiptop!";
+  public static String cheer(String hello) {
+    return hello;
   }
 
   public static void main(String[] args) {
-    System.out.println(cheer());
+    System.out.println(cheer("Hello rest of Tiptop!"));
   }
 }
